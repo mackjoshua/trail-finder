897fc5d226f3a82c5d087ab32f40c32055eb70b0
Joshua <joshmack02@gmail.com>
2023-03-06 10:31:23 -0800

    Image slider indexing was corrected

--- app/ImageSlider.jsx
+++ app/ImageSlider.jsx
@@ -11,28 +11,55 @@ const options = {
         Authorization: `${process.env.FS_KEY_2}`
     }
   };
-  
-  
 
+  
+// Need to make it so that currentSlide stays within the index range and doesn't get too far left or too far right
 export default function ImageSlider({slides}) {
+
+    const slidesLength = slides.length;
 
     const [currentSlide, setSlide] = useState(0);
 
     const pathname = usePathname();
     const fsqID = pathname.replace('/details/','');
-    console.log(slides);
+    console.log(slidesLength);
   
   
     console.log(fsqID);
 
+    function incrementSlide() {
+      if (currentSlide === (slidesLength - 1)) {
+        setSlide(0);
+      } else {
+        setSlide(currentSlide + 1);
+      }
+      
+    }
+
+    function decrementSlide() {
+
+      if (currentSlide === 0) {
+        setSlide(slidesLength - 1);
+      } else {
+        setSlide(currentSlide - 1);
+      }
+      
+    }
+
   return (
     <>
         <div>ImageSlider</div>
-        <button onClick={() => {setSlide(currentSlide + 1)}}>Next</button>
-        <button onClick={() => {setSlide(currentSlide - 1)}}>Previous</button>
+        <button onClick={incrementSlide}>Next</button>
+        <button onClick={decrementSlide}>Previous</button>
 
         <div>
-           <Image alt="Trails in the Santa Monica Mountains" src={`${slides[currentSlide].prefix}original${slides[currentSlide].suffix}`} width={slides[currentSlide].width} height={slides[currentSlide].height}/>
+           <Image alt="Trails in the Santa Monica Mountains" src={`${slides[currentSlide].prefix}100x100${slides[currentSlide].suffix}`} width={100} height={100}/>
+           {/* <Image alt="Trails in the Santa Monica Mountains" src={`${slides[currentSlide].prefix}original${slides[currentSlide].suffix}`} width={slides[currentSlide].width} height={slides[currentSlide].height}/> */}
+        </div>
+        <div>
+          {slides.map((slide, slideIndex) => ( 
+          <div key={slideIndex} onClick={() => {setSlide(slideIndex)}}>⬤</div>
+          ))}
         </div>
     </>
   )
