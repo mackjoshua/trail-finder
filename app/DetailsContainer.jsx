fca3799da9a2f4bd397315aefeff9606051dcff9
Joshua <joshmack02@gmail.com>
2023-03-11 11:17:23 -0800

    Client Side API Route fetch added

--- app/DetailsContainer.jsx
+++ app/DetailsContainer.jsx
@@ -2,7 +2,7 @@
 
 import React from 'react'
 import { usePathname, useRouter, useSearchParams } from 'next/navigation'
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const options = {
     method: 'GET',
@@ -16,7 +16,7 @@ const options = {
 
 export default function DetailsContainer() {
 
-
+  const [dataBrick, setDataBrick] = useState({});
 
   const router = useRouter();
   const searchParams = useSearchParams();
@@ -24,16 +24,30 @@ export default function DetailsContainer() {
 
   const id = router.query;
 
-  const fetchData = async () => {
+  
 
-    // For some reason, using a template string here wouldn't work. id was being read as ${search} instead of the dynamic value, so I had to concatenate otherwise
-    const URL = `http://localhost:3000/api/placedetails?id=` + search;
-    const res = await fetch(URL);
-  }
 
-  fetchData();
+  useEffect(() => {
+    const fetchData = async () => {
+
+      // For some reason, using a template string here wouldn't work. id was being read as ${search} instead of the dynamic value, so I had to concatenate otherwise
+      const URL = `http://localhost:3000/api/placedetails?id=` + search;
+      const res = await fetch(URL);
+      const data = await res.json();
+      setDataBrick(data);
+    };
+    fetchData();
+  },[search]);
+
+  useEffect(() => {
+    console.log(dataBrick, 'this is the dataBrick');
+  }, [dataBrick]);
+
   
   return (
-    <div>DetailsContainer</div>
+    <>
+      <div>DetailsContainer</div>
+      <div>{dataBrick.name} Here</div>
+    </>
   )
 }
--- pages/api/placedetails/index.js
+++ pages/api/placedetails/index.js
@@ -1,3 +1,5 @@
+// import fetch from 'node-fetch';
+
 const options = {
     method: 'GET',
     headers: {
@@ -9,8 +11,9 @@ const options = {
 export default async function handler(req, res) {
     const id = await req.query.id;
     console.log(id, 'here is the value of id, right here');
-    let data = await fetch(`https://api.foursquare.com/v3/places/${id}`, options)
+    let response = await fetch(`https://api.foursquare.com/v3/places/${id}`, options)
+    const data = await response.json();
     // console.log(await data.json());
-    console.log(await data.json())
-    res.status(200).json(); 
+    // console.log(await data.json())
+    res.status(200).json(data); 
 }
