630b369fde51687188d4dda33f6b98ffa4602adb
Joshua <joshmack02@gmail.com>
2023-04-03 20:10:57 -0700

    Removed getPhotos

--- app/details/[trail]/page.jsx
+++ app/details/[trail]/page.jsx
@@ -2,31 +2,31 @@ import TrailsPageWrapper from '@/app/TrailsPageWrapper';
 
 
 
-const options = {
-  method: 'GET',
-  headers: {
-      accept: 'application/json',
-      Authorization: `${process.env.FS_KEY_2}`
-  }
-};
+// const options = {
+//   method: 'GET',
+//   headers: {
+//       accept: 'application/json',
+//       Authorization: `${process.env.FS_KEY_2}`
+//   }
+// };
 
 
-const getPhotos = async () => {
-  const URL = 'http://localhost:3000/api/placesphotos';
-  const res = await fetch(URL, options);
-  const slides = await res.json();
-  return await slides;
-}
+// const getPhotos = async () => {
+//   const URL = 'http://localhost:3000/api/placesphotos';
+//   const res = await fetch(URL, options);
+//   const slides = await res.json();
+//   return await slides;
+// }
 
 
 
-const slides = await getPhotos();
+// const slides = await getPhotos();
 
 export default function Page() {
 
   return (
     <>
-        <TrailsPageWrapper slides={slides} />
+        <TrailsPageWrapper/>
     </>
   )
 }
