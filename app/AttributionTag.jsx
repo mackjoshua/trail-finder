c3046af4aeb9d6035fed79700707f1021449a1a5
Joshua <joshmack02@gmail.com>
2023-04-03 18:22:21 -0700

    Corrected attributionTag filename

--- app/AttributionTag.jsx
+++ app/AttributionTag.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import attrTag from 'Illustrations/PbFSQ_White.png'
-import styles from './CSS/AttributionTag.module.css'
+import styles from './CSS/attributionTag.module.css'
 
 export default function AttributionTag() {
   return (
