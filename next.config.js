50a06e8d1cda5f427e71357936c3ef9890598a6d
Joshua <joshmack02@gmail.com>
2023-04-04 12:58:18 -0700

    Modified config file for CORS

--- next.config.js
+++ next.config.js
@@ -19,6 +19,19 @@ const nextConfig = {
  webpack(config) {
   config.experiments = {...config.experiments, topLevelAwait: true }
   return config
+  },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "https://trail-finder-five.vercel.app" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
   }
 }
 
